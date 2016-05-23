b0cdc246ec967ce54de5ea76ae123641edd1691f
Stephen Sylvia <ssylvia@esri.com>
2016-05-23 13:17:35 -0400

    remove test credentials imports

--- src/app/main-app.js
+++ src/app/main-app.js
@@ -2,22 +2,12 @@
 window.define.amd.jQuery = true;
 
 require([
-  //----------------------------------------------
-  // Development - TODO to be removed for release
-  //----------------------------------------------
-  'babel/testCredentials',
-  //----------------------------------------------
   'react',
   'reactDom',
   'babel/components/crowdsource/CrowdsourceContainer',
   'babel/controllers/CrowdsourceController',
   'babel/config'
 ], function(
-  //----------------------------------------------
-  // Development - TODO to be removed for release
-  //----------------------------------------------
-  testCredentials,  // eslint-disable-line no-unused-vars
-  //----------------------------------------------
   React,
   ReactDOM,
   CrowdsourceApp,
