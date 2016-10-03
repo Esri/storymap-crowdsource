7521ded03c38fc40ffd019f3fa4aa29b653eaa7b
Stephen Sylvia <ssylvia@esri.com>
2016-10-03 15:00:46 -0400

    add language references to NLS files

--- src/resources/translations/builder/nls/template.js
+++ src/resources/translations/builder/nls/template.js
@@ -1,5 +1,5 @@
 define({
-  root: {
+  'root': ({
     // TODO Remove after beta
     betaMessage: {
       title: 'This is a beta release of Story Map Crowdsource',
@@ -456,5 +456,38 @@ define({
         }
       }
     }
-  }
+  }),
+  'ar': 1,
+  'bs': 1,
+  'cs': 1,
+  'da': 1,
+  'de': 1,
+  'es': 1,
+  'et': 1,
+  'fi': 1,
+  'fr': 1,
+  'he': 1,
+  'hr': 1,
+  'id': 1,
+  'it': 1,
+  'ja': 1,
+  'ko': 1,
+  'lt': 1,
+  'lv': 1,
+  'nl': 1,
+  'nb': 1,
+  'pl': 1,
+  'pt-br': 1,
+  'pt-pt': 1,
+  'ro': 1,
+  'ru': 1,
+  'su': 1,
+  'sr': 1,
+  'sv': 1,
+  'tr': 1,
+  'th': 1,
+  'vi': 1,
+  'zh-cn': 1,
+  'zh-hk': 1,
+  'zh-tw': 1
 });
